83fe5cfa01a06eb1aed4b879a1a0ef58d2b2b78c
xixihac <2512253073@qq.com>
2020-02-24 13:36:48 +0800

    Init repo

---
+++ src/test/java/com/study/boke/BokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.boke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
